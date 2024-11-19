9a6929b406316a330ecf08f00a9cabfbec8c9463
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2024-11-19 16:58:45 +0545

    Created a task and viewed also in same page

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,12 +2,42 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
+use Illuminate\Auth\Events\Validated;
 use Livewire\Component;
+use Livewire\Atrributes\Rule;
+use Livewire\Attributes\Rule as AttributesRule;
 
 class TodoList extends Component
 {
+    #[AttributesRule('required|min:3|max:50')]
+    public $name;
+
+    public $search;
+
+
+
+    public function create()
+    {
+        // dd('test');
+
+        $validated=$this->validateOnly('name');
+        Todo::create($validated);
+
+        $this->reset('name');
+
+        session()->flash('success','Created');
+
+
+
+    } 
     public function render()
     {
-        return view('livewire.todo-list');
+
+        return view('livewire.todo-list',
+    [
+        'todos'=>Todo::latest()->get()
+    ]);
     }
+
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
         </div>
     </div>
     <div id="content" class="mx-auto" style="max-width:500px;">
-      
+      @livewire('todo-list')
     </div>
 
 </body>
